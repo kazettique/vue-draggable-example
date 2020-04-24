5c4998cfcaf1b26be288953f558444f14c7f82f3
Woody <woody.chang@cloud-interactive.com>
2020-04-24 17:16:21 +0800

    first commit

---
+++ src/components/DraggableExample.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper">
+    <h2 class="title">Draggable example</h2>
+    <draggable v-model="taskList" class="bigger-area">
+      <transition-group name="list-complete">
+        <div
+          v-for="task in taskList"
+          :key="task.id"
+          class="drag-item flex flex-justify-betweeen"
+          @dragend="log('a')"
+        >
+          <strong>[IT-{{ task.id }}]</strong> {{ task.content }}
+        </div>
+      </transition-group>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+import { taskList } from "../initialTasks";
+
+export default {
+  name: "Drag",
+  data() {
+    return {
+      exampleList: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"],
+      taskList
+    };
+  },
+  components: {
+    draggable
+  },
+  methods: {
+    log: function(...e) {
+      console.log(...e);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper {
+  width: 20vw;
+  /* margin: 0 auto; */
+  height: auto;
+}
+.title {
+  margin-bottom: 2em;
+}
+.bigger-area > span {
+  /* min-height: 90vh; */
+  /* display: block; */
+}
+.drag-item {
+  padding: 15px 10px;
+  background-color: whitesmoke;
+  min-width: 30vw;
+  max-width: 90vw;
+  margin: 0 auto 10px;
+  cursor: -moz-grab;
+  cursor: -webkit-grab;
+  cursor: grab;
+  transition: transform 0.25s cubic-bezier(0.02, 1.01, 0.94, 1.01);
+  will-change: transform;
+}
+.list-complete-enter,
+.list-complete-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ src/initialTasks.js
@@ -0,0 +1,23 @@
+const initialTaskLists = [
+  [
+    { id: 100, content: "Code Refinement" },
+    { id: 102, content: "Study Vue" },
+    { id: 103, content: "Add RWD" }
+  ],
+  [
+    { id: 104, content: "Fix IE bugs" },
+    { id: 105, content: "Study React-DnD" }
+  ],
+  [{ id: 106, content: "Write testing" }]
+];
+
+const taskList = [
+  { id: 100, content: "Code Refinement" },
+  { id: 102, content: "Study Vue" },
+  { id: 103, content: "Add RWD" },
+  { id: 104, content: "Fix IE bugs" },
+  { id: 105, content: "Study React-DnD" },
+  { id: 106, content: "Write testing" }
+];
+
+export { initialTaskLists, taskList };
